f3afd636131426b90686a3687a93e5a2d0497757
seetsuko <radgjms@gmail.com>
2023-08-18 16:36:35 +0900

    Queryの設定

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,28 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
 
-const server = new ApolloServer({});
+
+// どういうふうにAPiに対して問い合わせをするのかを設定
+const typeDefs = gql`
+    # 型の定義
+    type Book {
+        title: String
+        author: String
+    }
+    # testというクエリを投げかけると上記のBookを配列として取得するように設定している。
+    type Query {
+        test: [Book]
+    }
+`;
+
+// 実際にQueryのtestが呼ばれたらどんな値を返すのかを設定
+const resolvers = {
+    // クエリが呼ばれたらtestが呼ばれて（関数と同じ感じ）、booksを返すようにしている。
+    Query: {
+        test: () => books,
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`server ready at ${url}`)
